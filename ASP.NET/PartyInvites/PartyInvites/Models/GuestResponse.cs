4c72a6fe3b8b0fe4f753d677032c248935ab8cd0
Miguel Rodriguez <miguel.rodriguez@rhinosystems.mx>
2018-07-05 19:12:55 -0500

    Proyecto de Curso ASP.NET y avance bootstrap

---
+++ ASP.NET/PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { set; get; }
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+",
+            ErrorMessage = "Please enter a valid email address")]
+        public string Email { set; get; }
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone { set; get; }
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        public bool? WillAttend { set; get; }
+    }
+}
