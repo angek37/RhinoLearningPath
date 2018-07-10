5f25374bb3fccf9cf96adb563cdd17da54341707
Miguel Rodriguez <miguel.rodriguez@rhinosystems.mx>
2018-07-10 18:31:22 -0500

    Prepara Proyecto de Capítulo 7

---
+++ ASP.NET/SportsStore/SportsStore.Domain/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace SportsStore.Domain.Entities
+{
+    public class Product
+    {
+        public int ProductID { set; get; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+        public decimal Price { set; get; }
+        public string Category { set; get; }
+    }
+}
