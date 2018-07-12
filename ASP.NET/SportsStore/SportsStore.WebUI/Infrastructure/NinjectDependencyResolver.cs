0c99de21471a2ee0d7ff818effcf41080301ce18
Miguel Rodriguez <miguel.rodriguez@rhinosystems.mx>
2018-07-11 19:17:00 -0500

    Termina capitulo 7 y comienza el 8
    Elabora el contexto para conectar EF a la base de datos de SQL Server
    Cambia el mock del Ninject por el repo de EF
    Agrega paginación
    Cambia routing
    Elabora UnitTests
    Desarolla Modelos de Vista

--- ASP.NET/SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ ASP.NET/SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -5,6 +5,7 @@ using System.Web.Mvc;
 using Moq;
 using Ninject;
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
@@ -31,15 +32,17 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns(new List<Product>
-            {
-                new Product { Name = "Football", Price = 25 },
-                new Product { Name = "Suft board", Price = 179 },
-                new Product { Name = "Running shoes", Price = 95 }
-            });
+            // Se cambia el mock por una implementación real
+            //Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            //mock.Setup(m => m.Products).Returns(new List<Product>
+            //{
+            //    new Product { Name = "Football", Price = 25 },
+            //    new Product { Name = "Suft board", Price = 179 },
+            //    new Product { Name = "Running shoes", Price = 95 }
+            //});
+            //kernel.Bind<IProductRepository>().ToConstant(mock.Object);
 
-            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
         }
     }
 }
---
+++ ASP.NET/SportsStore/SportsStore.WebUI/Models/ProductsListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Models
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { set; get; }
+        public PagingInfo PagingInfo { set; get; }
+        public string CurrentCategory { set; get; }
+    }
+}
