d6a0e3a69ec8a26b5a864246a99cd6ee03778f4e
Miguel Rodriguez <miguel.rodriguez@rhinosystems.mx>
2018-07-18 18:55:55 -0500

    Termina Proyecto Sports Store
    - Agrega un panel de administración con un CRUD
    - Agrega seguridad al panel de administración con un login
    - Permite agregar imagen al producto

--- ASP.NET/SportsStore/SportsStore.Domain/Entities/Product.cs
+++ ASP.NET/SportsStore/SportsStore.Domain/Entities/Product.cs
@@ -1,11 +1,24 @@
-﻿namespace SportsStore.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace SportsStore.Domain.Entities
 {
     public class Product
     {
+        [HiddenInput(DisplayValue = false)]
         public int ProductID { set; get; }
+        [Required(ErrorMessage = "Please enter a product name")]
         public string Name { set; get; }
+        [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Please enter a description")]
         public string Description { set; get; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         public decimal Price { set; get; }
+        [Required(ErrorMessage = "Please specify a category")]
         public string Category { set; get; }
+        
+        public byte[] ImageData { set; get; }
+        public string ImageMimeType { set; get; }
     }
 }
---
+++ ASP.NET/SportsStore/SportsStore.WebUI/Models/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsStore.WebUI.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string UserName { set; get; }
+
+        [Required]
+        public string Password { set; get; }
+    }
+}
