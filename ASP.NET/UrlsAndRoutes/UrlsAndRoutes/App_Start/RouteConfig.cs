0ee2207bca809bae800ce1de4e7590a7708bc209
Miguel Rodriguez <miguel.rodriguez@rhinosystems.mx>
2018-07-23 17:54:37 -0500

    Termina Capítulo 16 "Advanced Routing Features"

--- ASP.NET/UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
+++ ASP.NET/UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web.Mvc;
 using System.Web.Routing;
+using UrlsAndRoutes.Infrastructure;
 
 namespace UrlsAndRoutes
 {
@@ -23,7 +24,7 @@ namespace UrlsAndRoutes
 
             //routes.MapRoute("MyRoute", "{controller}/{action}/{id}",
             //    new {controller = "Home", action = "Index", id = "DefaultId"});
-            
+
             // ESPECIFICAR CUAL CONTROLADOR ELEGIR POR NAMESPACE, CUANDO SE REPITEN
             //var myRoute = routes.MapRoute("AddControllerRouter", "{controller}/{action}/{id}/{*catchall}",
             //    new { controller = "Home", action = "Index", id = UrlParameter.Optional },
@@ -55,7 +56,6 @@ namespace UrlsAndRoutes
             //    },
             //    new[] {"URLsAndRoutes.Controllers"});
 
-            routes.MapMvcAttributeRoutes(); // Enabling Attribute Routing
 
             // Ya no se ocupa el array para especificar el namespace
             // porque no hay ambiguedad de nombres de controllers
@@ -68,13 +68,34 @@ namespace UrlsAndRoutes
             //    },
             //    new[] { "UrlsAndRoutes.Controllers" });
 
-            routes.MapRoute("MyRoute", "{controller}/{action}/{id}",
-                new
-                {
-                    controller = "Home",
-                    action = "Index",
-                    id = UrlParameter.Optional
-                });
+            // Para que el id lo coloque en un segmento separado, elimino esta ruta
+            //routes.MapRoute("NewRoute", "App/Do{action}",
+            //    new {controller = "Home"});
+
+            //routes.MapRoute("MyRoute", "{controller}/{action}/{id}",
+            //    new
+            //    {
+            //        controller = "Home",
+            //        action = "Index",
+            //        id = UrlParameter.Optional
+            //    });
+
+            routes.RouteExistingFiles = true;
+
+            routes.MapMvcAttributeRoutes(); // Enabling Attribute Routing
+
+            routes.IgnoreRoute("Content/{filename}.html");
+
+            routes.Add(new Route("SayHello", new CustomRouteHandler()));
+
+            routes.Add(new LegacyRoute(
+                "~/articles/Windows_3.1_Overview.html",
+                "~/old/.NET_1.0_Class_Library"));
+
+            routes.MapRoute("MyRoute", "{controller}/{action}", null,
+                new[] {"UrlsAndRoutes.Controllers"});
+            routes.MapRoute("MyOtherRoute", "App/{action}", new {controller = "Home"},
+                new[] {"UrlsAndRoutes.Controllers"});
         }
     }
 }
--- ASP.NET/UrlsAndRoutes/UrlsAndRoutes/Controllers/HomeController.cs
+++ ASP.NET/UrlsAndRoutes/UrlsAndRoutes/Controllers/HomeController.cs
@@ -19,5 +19,33 @@ namespace UrlsAndRoutes.Controllers
             //ViewBag.CustomVariable = RouteData.Values["id"];
             return View();
         }
+
+        //public ViewResult MyActionMethod()
+        //{
+        //    string myActionUrl = Url.Action("Index", new { id = "MyID" });
+        //    string myRouteUrl = Url.RouteUrl(new
+        //    {
+        //        controller = "Home",
+        //        action = "Index"
+        //    });
+        //    return View();
+        //}
+
+        //Redirigir a otra acción en el mismo controller
+        //public RedirectToRouteResult MyActionMethod()
+        //{
+        //    return RedirectToAction("Index");
+        //}
+
+        // Enviar un redirect usando un URL Generado
+        public RedirectToRouteResult MyActionMethod()
+        {
+            return RedirectToRoute(new
+            {
+                controller = "Home",
+                Action = "Index",
+                id = "MyID"
+            });
+        }
     }
 }
