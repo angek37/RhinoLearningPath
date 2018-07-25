0a46f9f96e24a42a3b8b2de872d66a4b7d684c71
Miguel Rodriguez <miguel.rodriguez@rhinosystems.mx>
2018-07-25 12:36:10 -0500

    Termina Capítulo 17 "Controllers and actions"

---
+++ ASP.NET/ControllersAndActions/ControllersAndActions/Controllers/DerivedController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ControllersAndActions.Infrastructure;
+
+namespace ControllersAndActions.Controllers
+{
+    public class DerivedController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello from the DerivedController Index method";
+            return View("MyView");
+        }
+
+        public ActionResult ProduceOutput()
+        {
+            //if (Server.MachineName == "DELL-MIGUEL")
+            //{
+            //    //Response.Redirect("/Basic/Index");
+            //    // Remplaza por una implementación propia de un ActionResult
+            //    return new CustomRedirectResult
+            //    {
+            //        Url = "/Basic/Index"
+            //    };
+            //}
+            //else
+            //{
+            //    Response.Write("Machine name: " + Server.MachineName + ", Controller: Derived, Action: ProduceOutput");
+            //    return null;
+            //}
+            
+            //return new RedirectResult("/Basic/Index"); // Se sustituye por la implementación propia de Microsoft
+            // También puede sustituirse por el método Convenience
+            return Redirect("/Basic/Index");
+        }
+    }
+}
---
+++ ASP.NET/ControllersAndActions/ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public ViewResult Index()
+        {
+            //DateTime date = DateTime.Now;
+            //return View(date);
+            ViewBag.Message = "Hello";
+            ViewBag.Date = DateTime.Now;
+            return View();
+        }
+
+        // Usar literales no es la forma más correcta
+        // un cambio en el schema de rutas y causa conflicto
+        //public RedirectResult Redirect()
+        //{
+        //    return Redirect("/Example/Index");
+        //}
+
+        // También podemos redirigir al action method directamente
+        //public RedirectToRouteResult Redirect()
+        //{
+        //    return RedirectToRoute(new
+        //    {
+        //        controller = "Example",
+        //        action = "Index",
+        //        Id = "MyID"
+        //    });
+        //}
+
+        public RedirectToRouteResult Redirect()
+        {
+            return RedirectToAction("Index", "Basic");
+        }
+
+        public HttpStatusCodeResult StatusCode()
+        {
+            //return new HttpStatusCodeResult(404, "URL cannot be serviced");
+            // Se puede crear el mismo efecto usando 
+            //return HttpNotFound();
+            // Lanzar error 401 request Unauthorized
+            return new HttpUnauthorizedResult();
+        }
+    }
+}
