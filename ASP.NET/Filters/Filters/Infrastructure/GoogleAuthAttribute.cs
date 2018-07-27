96ddf7a9d80f7ee231eadb56cbbd9d8bdae2a17f
Miguel Rodriguez <miguel.rodriguez@rhinosystems.mx>
2018-07-27 10:21:37 -0500

    Comienza Capítulo 18 "Filters"

---
+++ ASP.NET/Filters/Filters/Infrastructure/GoogleAuthAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+using System.Web.Routing;
+
+namespace Filters.Infrastructure
+{
+    public class GoogleAuthAttribute : FilterAttribute, IAuthenticationFilter
+    {
+        public void OnAuthentication(AuthenticationContext filterContext)
+        {
+            
+        }
+
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            if (filterContext.Result == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary
+                {
+                    {"controller", "GoogleAccount"},
+                    {"action", "Login"},
+                    {"returnUrl", filterContext.HttpContext.Request.RawUrl}
+                });
+            }
+        }
+    }
+}
