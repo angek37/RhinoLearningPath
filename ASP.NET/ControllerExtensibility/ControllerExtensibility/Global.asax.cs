ff96ee8a3119699f236290447f1c4e9509d068ce
Miguel Rodriguez <miguel.rodriguez@rhinosystems.mx>
2018-07-31 17:01:56 -0500

    Termina capítulo 19 "Controller Extensibility"

---
+++ ASP.NET/ControllerExtensibility/ControllerExtensibility/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using ControllerExtensibility.Infrastructure;
+
+namespace ControllerExtensibility
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            // Custom Controller Factory
+            //ControllerBuilder.Current.SetControllerFactory(new CustomControllerFactory());
+
+            // Built-in Controller Factory
+            // Prioritizing Namespaces
+            ControllerBuilder.Current.DefaultNamespaces.Add("MyControllerNamespace");
+            ControllerBuilder.Current.DefaultNamespaces.Add("MyController.*");
+
+            // Use Custom Activator
+            ControllerBuilder.Current.SetControllerFactory(
+                new DefaultControllerFactory(new CustomControllerActivator()));
+        }
+    }
+}
---
+++ ASP.NET/ControllerExtensibility/ControllerExtensibility/Models/Result.cs
@@ -0,0 +1,8 @@
+﻿namespace ControllerExtensibility.Models
+{
+    public class Result
+    {
+        public string ControllerName { set; get; }
+        public string ActionName { set; get; }
+    }
+}
