88abf409f472f8bc3b37d9cf9d7a84e71cc65d79
Miguel Rodriguez <miguel.rodriguez@rhinosystems.mx>
2018-08-01 17:49:24 -0500

    Termina capítulo 21 "Helper methods"

---
+++ ASP.NET/HelperMethods/HelperMethods/Models/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HelperMethods.Models
+{
+    public class Person
+    {
+        public int PersonId { set; get; }
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+        public DateTime BirthDate { set; get; }
+        public Address HomeAddress { set; get; }
+        public bool IsApproved { set; get; }
+        public Role Role { set; get; }
+    }
+
+    public class Address
+    {
+        public string Line1 { set; get; }
+        public string Line2 { set; get; }
+        public string City { set; get; }
+        public string PostalCode { set; get; }
+        public string Country { set; get; }
+    }
+
+    public enum Role
+    {
+        Admin,
+        User,
+        Guest
+    }
+}
