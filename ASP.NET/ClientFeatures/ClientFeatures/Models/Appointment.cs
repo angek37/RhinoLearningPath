195572dbc70e5d1585d2ca5a46780a3d65487823
Miguel Rodriguez <miguel.rodriguez@rhinosystems.mx>
2018-08-06 13:31:49 -0500

    Termina capítulo 26 "Bundles"

---
+++ ASP.NET/ClientFeatures/ClientFeatures/Models/Appointment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClientFeatures.Models
+{
+    public class Appointment
+    {
+        [Required]
+        public string ClientName { set; get; }
+        public bool TermsAccepted { set; get; }
+    }
+}
