1fabf98c1bcc853803aaafdb822779d180285bae
Miguel Rodriguez <miguel.rodriguez@rhinosystems.mx>
2018-08-06 11:38:09 -0500

    Termina capítulo 25 "Model Validation"

---
+++ ASP.NET/ModelValidation/ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Web.Mvc;
+using ModelValidation.Models;
+
+namespace ModelValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult MakeBooking()
+        {
+            return View(new Appointment {Date = DateTime.Now});
+        }
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appt)
+        {
+            // statements to store new Appointment in a
+            // repository would go here in a real project
+            if (ModelState.IsValid)
+            {
+                return View("Completed", appt);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public JsonResult ValidateDate(string Date)
+        {
+            DateTime parsedDate;
+            if (!DateTime.TryParse(Date, out parsedDate))
+            {
+                return Json("Please enter a valid date (dd/mm/yyyy)",
+                    JsonRequestBehavior.AllowGet);
+            }
+            else if (DateTime.Now > parsedDate)
+            {
+                return Json("Please enter a date in the future",
+                    JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ ASP.NET/ModelValidation/ModelValidation/Models/Appointment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using ModelValidation.Infrastructure;
+using System.Web.Mvc;
+
+namespace ModelValidation.Models
+{
+    [NoJoeOnMondays]
+    public class Appointment
+    {
+        [Required]
+        [StringLength(10, MinimumLength = 3)]
+        public string ClientName { set; get; }
+        [DataType(DataType.Date)]
+        //[Required(ErrorMessage = "Please enter a date")]
+        // Custom Model Validation Attribute 
+        //[FutureDate(ErrorMessage = "Please enter a date in the future")]
+        [Remote("ValidateDate", "Home")]
+        public DateTime Date { set; get; }
+        //[Range(typeof(bool), "true", "true", ErrorMessage = "You must accept the terms")]
+        // Custom Validation Attribute
+        [MustBeTrue(ErrorMessage = "You must accept the terms")]
+        public bool TermsAccepted { set; get; }
+    }
+}
